bfe38ba0267a6ee5439bc3a9099538ce5a29057a
Master <blackant.master.am@gmail.com>
2023-09-11 13:24:22 +0200

    Update WebGLMicrophone.cs

--- Runtime/Scripts/WebGLMicrophone.cs
+++ Runtime/Scripts/WebGLMicrophone.cs
@@ -1,12 +1,22 @@
-using System.Collections;
-using System.Collections.Generic;
+#if UNITY_WEBGL && !UNITY_EDITOR
+#define USE_WEBGL
+#endif
+
+using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
 using UnityEngine;
+
+#if USE_WEBGL
+using System.Runtime.InteropServices;
+#endif
 
 namespace Flippit
 {
     public static class WebGLMicrophone 
     {
-        #if USE_WEBGL
+#if USE_WEBGL
         [DllImport("__Internal")]
         public static extern void MicrophoneWebGL_Start(string deviceName, bool loop, int lengthSec, int frequency, int channelCount, Action<string> callback, Action<string> deleteClip);
 
@@ -31,6 +41,36 @@ namespace Flippit
         [DllImport("__Internal")]
         private static extern bool MicrophoneWebGL_HasPermission(string deviceName);
 #endif
+        private delegate void FetchResultDelegate(string result);
+
+        private static TaskCompletionSource<string> _taskCompletionSource;
+
+        private static Task<string> FetchAsync()
+        {
+            _taskCompletionSource = new TaskCompletionSource<string>();
+#if USE_WEBGL
+            MicrophoneWebGL_FetchMicrophoneDevices(ResolveFetch, RejectFetch);
+#endif
+            return _taskCompletionSource.Task;
+        }
+
+        public static async Task<string[]> MicrophoneWebGL_Devices()
+        {
+            var jsonTask = await FetchAsync();
+            return jsonTask.Split('|');
+        }
+
+        [AOT.MonoPInvokeCallback(typeof(FetchResultDelegate))]
+        private static void ResolveFetch(string result)
+        {
+            _taskCompletionSource.SetResult(result);
+        }
+
+        [AOT.MonoPInvokeCallback(typeof(FetchResultDelegate))]
+        private static void RejectFetch(string error)
+        {
+            _taskCompletionSource.SetException(new Exception(error));
+        }
     }
 
 }
